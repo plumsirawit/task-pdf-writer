c86cde8a87c9fe7031b5c4ad9d42ec1a0e21a11f
Sirawit Pongnakintr <pplluurrmm2545@hotmail.com>
2021-07-13 12:24:20 +0700

    fix(pdf): use base64 on response

--- aws-node-api/handler-pdf.ts
+++ aws-node-api/handler-pdf.ts
@@ -3,14 +3,12 @@ import express from "express";
 import serverless from "serverless-http";
 import wkhtmltopdf from "wkhtmltopdf";
 import { spawn, execSync } from "child_process";
-//wkhtmltopdf.command = "/opt/bin/wkhtmltopdf";
-wkhtmltopdf.shell = "bash";
+wkhtmltopdf.command = "/opt/bin/wkhtmltopdf";
 
 import fs from "fs";
-import path from "path";
 const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
 app.get("/pdf/hello", async function (req, res) {
   res.status(200).json({ message: "Hello, world!" });
@@ -25,8 +23,10 @@ app.get("/pdf/generate", async function (req, res) {
       .pipe(fs.createWriteStream(file))
       .on("finish", () => reso())
   );
-  res.download(file);
-  // res.status(501).json({ error: "not yet implemented" });
+  res.send(fs.readFileSync(file, { encoding: 'base64' }))
+  // use this because res.sendFile and res.download seems to be buggy
+  // bug: file content got corrupted (i.e. FE FF turns into EF BF BD EF BF BD)
+  // no idea why is this happening
 });
 
 app.get("/pdf/readfiles", async function (req, res) {
@@ -54,11 +54,16 @@ app.get("/pdf/manual", async function (req, res) {
   if (input) {
     args.push(isUrl ? quote(input) : "-");
   }
-  args.push("C:/tmp/out-bbb.pdf");
-  const child = execSync(
-    [wkhtmltopdf.shell, "-c", "set -oe pipefail ; " + args.join(" ")].join(" ")
+  args.push("/tmp/out-bbb.pdf");
+  const child = spawn(
+    wkhtmltopdf.shell, ["-c", "set -oe pipefail ; " + args.join(" ")]
   );
-  res.download("C:/tmp/out-bbb.pdf");
+  child.stdout.pipe(process.stdout)
+  child.stderr.pipe(process.stderr)
+  child.on('exit', (code) => {
+    console.log('code', code)
+    res.send(fs.readFileSync("/tmp/out-bbb.pdf", { encoding: 'base64' }))
+  })
 });
 
 app.use((req, res, next) => {
