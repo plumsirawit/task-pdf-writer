aa1c46c826ac4e70576091542a2bc8f1935d4258
Sirawit Pongnakintr <plurm2545@hotmail.com>
2021-07-13 17:42:33 +0700

    feat: katex math rendering

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import "../styles/globals.css";
 import "../styles/markdown.css";
 import "../styles/code.css";
+
+import "easymde/dist/easymde.min.css";
+import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,12 +1,11 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
   ssr: false,
 });
-import "easymde/dist/easymde.min.css";
 import styles from "../styles/Editor.module.css";
 import dynamic from "next/dynamic";
 import marked from "../utils/initMarked";
-
+import renderMathInElement from "katex/dist/contrib/auto-render";
 export default function Editor() {
   const outputRef = useRef<HTMLDivElement>(null);
   const [markdownInput, setMarkdownInput] = useState<string>("hello, world");
@@ -15,12 +14,31 @@ export default function Editor() {
     if (!outputDiv) {
       return;
     }
-    outputDiv.innerHTML = marked(markdownInput);
+    outputDiv.innerHTML = marked(markdownInput.replaceAll(/\\/g, "\\\\"));
+    renderMathInElement(outputDiv, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "\\[", right: "\\]", display: true },
+        { left: "$", right: "$", display: false },
+        { left: "\\(", right: "\\)", display: false },
+      ],
+      throwOnError: false,
+    });
   }, [markdownInput]);
   return (
     <div className="row">
       <div className={`${styles["col-6"]} ${styles["edit-pane"]}`}>
-        <SimpleMDE value={markdownInput} onChange={setMarkdownInput} />
+        <SimpleMDE
+          onChange={setMarkdownInput}
+          options={useMemo(
+            () => ({
+              toolbar: false,
+              spellChecker: false,
+              status: false,
+            }),
+            []
+          )}
+        />
       </div>
       <div
         className={`${styles["col-6"]} ${styles["preview-pane"]} ${styles["markdown-body"]} markdown-body`}
