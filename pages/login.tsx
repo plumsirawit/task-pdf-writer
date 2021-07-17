f177236ccf23af62a2109f474a3f51aafcd1bbd4
Sirawit Pongnakintr <plurm2545@hotmail.com>
2021-07-17 09:26:38 +0700

    refactor(login): use components

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,11 +1,14 @@
-import { withAuthUser, useAuthUser, AuthAction } from "next-firebase-auth";
+import { withAuthUser, AuthAction } from "next-firebase-auth";
 import Head from "next/head";
 import styles from "../styles/Login.module.css";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
-import { useEffect, useMemo, useState } from "react";
-import { MoonLoader } from "react-spinners";
+import { useState } from "react";
+import { Card } from "../components/Card";
+import { Input, InputHead } from "../components/Input";
+import { Button } from "../components/Button";
+import { Spinner } from "../components/Spinner";
 export default withAuthUser({
   whenAuthed: AuthAction.REDIRECT_TO_APP,
 })(function Login() {
@@ -33,35 +36,23 @@ export default withAuthUser({
 
         <p className={styles.description}>Welcome back!</p>
 
-        <div className={styles.card}>
-          <h3 className={styles.inputhead}>Email</h3>
-          <input
+        <Card>
+          <InputHead>Email</InputHead>
+          <Input
             type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className={styles.input}
           />
-          <h3 className={styles.inputhead}>Password</h3>
-          <input
+          <InputHead>Password</InputHead>
+          <Input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
           />
-          <button
-            className={styles.button}
-            onClick={register}
-            disabled={isLoading}
-          >
-            {isLoading ? (
-              <div className={styles.spinnerwrapper}>
-                <MoonLoader size="15px" color="white" css="display: block" />
-              </div>
-            ) : (
-              <>Sign in</>
-            )}
-          </button>
-        </div>
+          <Button onClick={register} disabled={isLoading}>
+            {isLoading ? <Spinner /> : <>Sign in</>}
+          </Button>
+        </Card>
       </main>
     </div>
   );
