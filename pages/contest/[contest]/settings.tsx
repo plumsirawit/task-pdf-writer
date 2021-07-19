0712d0e59b9818a1b40feebd395344e8be9e38c7
Sirawit Pongnakintr <plurm2545@hotmail.com>
2021-07-19 08:15:35 +0700

    fix: update logo on change

--- pages/contest/[contest]/settings.tsx
+++ pages/contest/[contest]/settings.tsx
@@ -14,138 +14,7 @@ const Input = styled(DefaultInput)`
   margin-bottom: 5px;
 `;
 
-interface ISettingsFormProps {
-  contestFullTitle: string;
-  contestTitle: string;
-  contest: string;
-  country: string;
-  language: string;
-  languageCode: string;
-  contestDate: string;
-  logo: string;
-  contestId: string;
-}
-
-const SettingsForm = (props: ISettingsFormProps) => {
-  const [contestFullTitle, setContestFullTitle] = useState<string>(
-    props.contestFullTitle
-  );
-  const [contestTitle, setContestTitle] = useState<string>(props.contestTitle);
-  const [contest, setContest] = useState<string>(props.contest);
-  const [country, setCountry] = useState<string>(props.country);
-  const [language, setLanguage] = useState<string>(props.language);
-  const [languageCode, setLanguageCode] = useState<string>(props.languageCode);
-  const [contestDate, setContestDate] = useState<string>(props.contestDate);
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  useEffect(() => {
-    setContestFullTitle(props.contestFullTitle);
-    setContestTitle(props.contestTitle);
-    setContest(props.contest);
-    setCountry(props.country);
-    setLanguage(props.language);
-    setLanguageCode(props.languageCode);
-    setContestDate(props.contestDate);
-  }, [props]);
-  const authUser = useAuthUser();
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const submitForm = async () => {
-    setIsLoading(true);
-    const fileInput = fileInputRef.current;
-    if (!fileInput) {
-      alert("file input not loaded");
-      setIsLoading(false);
-      return;
-    }
-    const file = fileInput.files?.[0];
-    const response = await callUpdateContestApi(authUser, {
-      contestFullTitle,
-      contestTitle,
-      contest,
-      country,
-      language,
-      languageCode,
-      contestDate,
-      logo: file && (await toBase64(file)),
-      contestId: props.contestId,
-    });
-    if (response?.error) {
-      alert(response.error);
-      setIsLoading(false);
-      return;
-    }
-    setIsLoading(false);
-  };
-  return (
-    <>
-      <h3>Full Title</h3>
-      <Input
-        type="text"
-        value={contestFullTitle}
-        onChange={(e) => setContestFullTitle(e.target.value)}
-        placeholder="International Olympiad in Informatics 2019"
-      />
-      <h3>Short Title</h3>
-      <Input
-        type="text"
-        value={contestTitle}
-        onChange={(e) => setContestTitle(e.target.value)}
-        placeholder="IOI 2019"
-      />
-      <h3>Name</h3>
-      <Input
-        type="text"
-        value={contest}
-        onChange={(e) => setContest(e.target.value)}
-        placeholder="Day 1"
-      />
-      <h3>Country Code</h3>
-      <Input
-        type="text"
-        value={country}
-        onChange={(e) => setCountry(e.target.value)}
-        placeholder="THA"
-      />
-      <h3>Language</h3>
-      <Input
-        type="text"
-        value={language}
-        onChange={(e) => setLanguage(e.target.value)}
-        placeholder="Thai"
-      />
-      <h3>Language Code</h3>
-      <Input
-        type="text"
-        value={languageCode}
-        onChange={(e) => setLanguageCode(e.target.value)}
-        placeholder="THA"
-      />
-      <h3>Contest Date</h3>
-      <Input
-        type="text"
-        value={contestDate}
-        onChange={(e) => setContestDate(e.target.value)}
-        placeholder="September 4, 2019"
-      />
-      <h3>Logo</h3>
-      <div className={styles.row}>
-        <span>Current Logo:</span>
-        <img src={props.logo} className={styles.logo} />
-      </div>
-      <Input type="file" ref={fileInputRef} multiple={false} />
-      <Button
-        onClick={submitForm}
-        style={{ marginBottom: 40 }}
-        disabled={isLoading}
-      >
-        Update
-      </Button>
-    </>
-  );
-};
-
-export default withAuthUser({
-  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
-})(function Contest() {
+const SettingsForm = () => {
   const router = useRouter();
   const contestId = useContestId();
   const [contentReady, setContentReady] = useState<boolean>(false);
@@ -156,7 +25,7 @@ export default withAuthUser({
   const [language, setLanguage] = useState<string>("");
   const [languageCode, setLanguageCode] = useState<string>("");
   const [contestDate, setContestDate] = useState<string>("");
-  const [logo, setLogo] = useState<string>("https://picsum.photos/300/200"); // base64
+  const [logo, setLogo] = useState<string>(""); // base64
   useEffect(() => {
     if (!contestId) {
       return;
@@ -187,29 +56,142 @@ export default withAuthUser({
         alert(e.message);
         router.push("/contests");
       });
-  });
+  }, [contestId]);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const authUser = useAuthUser();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const submitForm = async () => {
+    setIsLoading(true);
+    const fileInput = fileInputRef.current;
+    if (!fileInput) {
+      alert("file input not loaded");
+      setIsLoading(false);
+      return;
+    }
+    if (!contestId) {
+      alert("contest id not loaded");
+      setIsLoading(false);
+      return;
+    }
+    const file = fileInput.files?.[0];
+    const response = await callUpdateContestApi(authUser, {
+      contestFullTitle,
+      contestTitle,
+      contest,
+      country,
+      language,
+      languageCode,
+      contestDate,
+      logo: file && (await toBase64(file)),
+      contestId,
+    });
+    if (response?.error) {
+      alert(response.error);
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (!fileInputRef.current) {
+      return;
+    }
+    const fileInput = fileInputRef.current;
+    const cb = async () => {
+      const file = fileInput.files?.[0];
+      const newLogo = file && (await toBase64(file));
+      if (newLogo) {
+        setLogo(newLogo);
+      }
+    };
+    fileInput.addEventListener("change", cb);
+    return () => fileInput.removeEventListener("change", cb);
+  }, [fileInputRef, fileInputRef.current]);
   return (
     <>
       {contentReady && contestId && (
-        <div className={styles.container}>
-          <div className={styles.topbar}>
-            <h1 className={styles.title}>Contest Settings</h1>
+        <>
+          <h3>Full Title</h3>
+          <Input
+            type="text"
+            value={contestFullTitle}
+            onChange={(e) => setContestFullTitle(e.target.value)}
+            placeholder="International Olympiad in Informatics 2019"
+          />
+          <h3>Short Title</h3>
+          <Input
+            type="text"
+            value={contestTitle}
+            onChange={(e) => setContestTitle(e.target.value)}
+            placeholder="IOI 2019"
+          />
+          <h3>Name</h3>
+          <Input
+            type="text"
+            value={contest}
+            onChange={(e) => setContest(e.target.value)}
+            placeholder="Day 1"
+          />
+          <h3>Country Code</h3>
+          <Input
+            type="text"
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+            placeholder="THA"
+          />
+          <h3>Language</h3>
+          <Input
+            type="text"
+            value={language}
+            onChange={(e) => setLanguage(e.target.value)}
+            placeholder="Thai"
+          />
+          <h3>Language Code</h3>
+          <Input
+            type="text"
+            value={languageCode}
+            onChange={(e) => setLanguageCode(e.target.value)}
+            placeholder="THA"
+          />
+          <h3>Contest Date</h3>
+          <Input
+            type="text"
+            value={contestDate}
+            onChange={(e) => setContestDate(e.target.value)}
+            placeholder="September 4, 2019"
+          />
+          <h3>Logo</h3>
+          <div className={styles.row}>
+            <span>Current Logo:</span>
+            <img src={logo} className={styles.logo} />
           </div>
-          <div className={styles.panelcontainer}>
-            <SettingsForm
-              contestFullTitle={contestFullTitle}
-              contestTitle={contestTitle}
-              contest={contest}
-              country={country}
-              language={language}
-              languageCode={languageCode}
-              contestDate={contestDate}
-              logo={logo}
-              contestId={contestId}
-            />
-          </div>
-        </div>
+          <Input type="file" ref={fileInputRef} multiple={false} />
+          <Button
+            onClick={submitForm}
+            style={{ marginBottom: 40 }}
+            disabled={isLoading}
+          >
+            Update
+          </Button>
+        </>
       )}
+    </>
+  );
+};
+
+export default withAuthUser({
+  whenUnauthedAfterInit: AuthAction.REDIRECT_TO_LOGIN,
+})(function Contest() {
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.topbar}>
+          <h1 className={styles.title}>Contest Settings</h1>
+        </div>
+        <div className={styles.panelcontainer}>
+          <SettingsForm />
+        </div>
+      </div>
     </>
   );
 });
