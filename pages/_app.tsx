4525a9d8bbaba2fcbcfe8ace3819f422fde7c2df
Sirawit <plurm2545@hotmail.com>
2022-09-05 19:45:34 +0200

    feat: major version upgrade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,27 +7,10 @@ import "katex/dist/katex.min.css";
 import type { AppProps } from "next/app";
 
 import initAuth from "../initAuth";
-import Head from "next/head";
 
 initAuth();
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Oxygen&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 export default MyApp;
