a0ded3c4833854105581a36e8f3738056c77c68b
Sirawit <plurm2545@hotmail.com>
2022-09-06 19:00:03 +0200

    feat: update notes

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,11 +8,6 @@ import { FloatingButton } from "../components/FloatingButton";
 import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    alert(
-      "UNDER MAINTENANCE! Contest creation is disabled, and logo uploads are not guaranteed to be safe. Please use with caution. Planned next stable ver: 9 Sep 2022."
-    );
-  }, []);
   return (
     <div className={styles.container}>
       <Head>
